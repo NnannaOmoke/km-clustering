1df6b09a9c7f0229bea35c2e55effc6deda4ef10
Nnanna Omoke <omokennanna832@gmail.com>
2023-07-05 16:27:39 +0100

    Deleted the useless global vars

--- modular_quantization.py
+++ modular_quantization.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plotter
 import matplotlib.image as imager
 from sklearn.cluster import KMeans
 from PIL import Image as pil_imager
@@ -13,7 +12,6 @@ def read_process(path):
 
 def quantizer(colors, packed_val):
     learner = KMeans(n_clusters = colors, n_init = "auto")
-    n_colors = colors
     label = learner.fit_predict(packed_val[0])
     centers = learner.cluster_centers_.round(0).astype(int)
     quantized_img = np.reshape(centers[label], packed_val[1])
